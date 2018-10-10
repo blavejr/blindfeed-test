348e5d5e7e253ba15e793e3148b4eeb758536bc1
blavejr <kalimbatech@gmail.com>
2018-10-10 19:52:02 +0200

    added some packages to package.json that I had installed glonally

--- blindfeed/server.js
+++ blindfeed/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
+const history = require('connect-history-api-fallback');
 const app = express();
 app.use(history());
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
